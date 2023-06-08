888e2fa889664464408dd12f7d411ebe96685a76
Vitalii Plevako <vitalii.plevako@plus4u.net>
2023-06-08 20:38:47 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+  const style = {
+    color: 'green',
+    fontSize: 16,
+    fontStyle: 'italic'
+
+  }
+
+
+  return (
+    <div style={style}>
+      <em>Note app, Department of Computer Science, University of Helsinki 2022</em>
+    </div>
+  );
+}
+
+export default Footer;
