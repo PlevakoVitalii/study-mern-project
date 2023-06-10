b27a955b674217a7688fa7b8d93f1e1296e5afc2
Vitalii Plevako <vitalii.plevako@plus4u.net>
2023-06-10 16:16:33 +0300

    change path in get method for first render

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import style from './style.css'
 
 import App from './App'
 
-axios.get('http://localhost:3001/api/notes').then(response => {
+axios.get('/api/notes').then(response => {
   const notes = response.data
   ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} style={style} />)
 })
